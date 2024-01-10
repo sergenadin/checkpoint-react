d94a8e332a61162a1a8bc15682bc78b88ff6d5df
sergenadin <150278010+sergenadin@users.noreply.github.com>
2024-01-10 11:44:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Card from 'react-bootstrap/Card';
+
+function Mycard() {
+  return <Card body>This is some text within a card body.</Card>;
+}
+
+
+function Hearder() {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>
+          Change this and that and try again. Duis mollis, est non commodo
+          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+          Cras mattis consectetur purus sit amet fermentum.
+        </p>
+      </Alert>
+    );
+  }
+  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+}
+
+function Mynavbar() {
+  return (
+    <Navbar className="bg-body-tertiary justify-content-between">
+      <Form inline>
+        <InputGroup>
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <Form.Control
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+      </Form>
+      <Form inline>
+        <Row>
+          <Col xs="auto">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2"
+            />
+          </Col>
+          <Col xs="auto">
+            <Button type="submit">Submit</Button>
+          </Col>
+        </Row>
+      </Form>
+    </Navbar>
+  );
+}
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+   <Mynavbar /> 
+   <Hearder />
+   <Mycard/>
+   <Mycard/>
+   <Mycard/>
+    </>
+  )
+
+}
+
+
+export default App;
